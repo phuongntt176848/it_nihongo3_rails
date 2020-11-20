be69b705c18984beb43ba476215bda1a5913bd1d
phuongntt176848 <ubuntu@ip-172-31-9-46.ec2.internal>
2020-11-20 17:19:22 +0000

    Add avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  has_one_attached :avatar
+
+         
   validates :name, presence: true
   validates :age, presence: true
+  
 end
